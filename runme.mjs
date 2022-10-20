085eb10e450e74b7d7fc3f7c059f8f286b2651eb
Peet Gouws <peet@gouws.com>
2022-10-20 14:17:54 +0200

    Updated to run based on a periodic timer

--- runme.mjs
+++ runme.mjs
@@ -1,6 +1,6 @@
 import { getToken, getStationIds } from "./src/solarman.js";
-
 import { processStation } from "./src/processing.mjs";
+import dotenv from "dotenv";
 
 const single_run = (stationList) => {
   stationList.forEach(async (stationId, index) => {
@@ -8,6 +8,8 @@ const single_run = (stationList) => {
   });
 };
 
+dotenv.config();
+const { refresh_time } = process.env;
 await getToken();
 const stationList = await getStationIds();
 single_run(stationList);
@@ -15,4 +17,4 @@ var intervalId = setInterval(function () {
   // call your function here
   console.log("Checking Data");
   single_run(stationList);
-}, 60000);
+}, refresh_time * 60000);
--- src/mqtt.mjs
+++ src/mqtt.mjs
@@ -8,19 +8,22 @@ console.log(`mqtt:/${mqtt_broker}:${mqtt_port}/`);
 let client = connect(`mqtt:/${mqtt_broker}:${mqtt_port}/`, {
   username: mqtt_username,
   password: mqtt_password,
+  host: mqtt_broker,
+  port: mqtt_port,
 }); // create a client
 
 client.on("connect", () => {
-  console.log("connected to mqtt");
+  console.log("connect flag:" + client.connected);
 });
+client.on("error", (err) => console.log(err));
 
 const sendData = (subtopic, data) => {
-  const discard = ["code", "msg", "success", "requestId"];
-  for (const key in data) {
-    if (!discard.includes(key) && String(data[key]) != "null") {
-      client.publish(`${mqtt_topic}/${subtopic}/${key}`, String(data[key]));
-    }
-  }
+  client.publish(`${mqtt_topic}/${subtopic}`, JSON.stringify(data));
+  // const discard = ["code", "msg", "success", "requestId"];
+  // for (const key in data) {
+  //   if (!discard.includes(key) && String(data[key]) != "null") {
+  //     client.publish(`${mqtt_topic}/${subtopic}/${key}`, String(data[key]));
+  //   }
+  // }
 };
-
 export { sendData };
