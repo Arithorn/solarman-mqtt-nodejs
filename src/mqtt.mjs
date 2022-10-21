5d45d285aab1e74f05f9823f67c8f7b8658cc152
Peet Gouws <peet@gouws.com>
2022-10-21 07:28:07 +0200

    Basic Error Handling Added

--- src/mqtt.mjs
+++ src/mqtt.mjs
@@ -4,26 +4,23 @@ import { connect } from "mqtt"; // import connect from mqtt
 dotenv.config();
 const { mqtt_broker, mqtt_port, mqtt_topic, mqtt_username, mqtt_password } =
   process.env;
-console.log(`mqtt:/${mqtt_broker}:${mqtt_port}/`);
-let client = connect(`mqtt:/${mqtt_broker}:${mqtt_port}/`, {
+console.log(`Connecting to ${mqtt_broker} on port ${mqtt_port}`);
+const client = connect(`mqtt:/${mqtt_broker}:${mqtt_port}/`, {
   username: mqtt_username,
   password: mqtt_password,
   host: mqtt_broker,
   port: mqtt_port,
 }); // create a client
-
 client.on("connect", () => {
-  console.log("connect flag:" + client.connected);
+  console.log("Connected to Broker");
 });
 client.on("error", (err) => console.log(err));
 
 const sendData = (subtopic, data) => {
-  client.publish(`${mqtt_topic}/${subtopic}`, JSON.stringify(data));
-  // const discard = ["code", "msg", "success", "requestId"];
-  // for (const key in data) {
-  //   if (!discard.includes(key) && String(data[key]) != "null") {
-  //     client.publish(`${mqtt_topic}/${subtopic}/${key}`, String(data[key]));
-  //   }
-  // }
+  try {
+    client.publish(`${mqtt_topic}/${subtopic}`, JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+  }
 };
 export { sendData };
--- src/solarman.js
+++ src/solarman.js
@@ -2,45 +2,65 @@ require("dotenv").config();
 const api = require("./api/solarman");
 
 const getToken = async () => {
-  const { appid, secret, username, passhash, orgid } = process.env;
-  const result = await api.post(`/account/v1.0/token?appId=${appid}`, {
-    appSecret: secret,
-    email: username,
-    orgId: orgid,
-    password: passhash,
-  });
-  const { access_token } = result.data;
-  api.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
-  return access_token;
+  try {
+    const { appid, secret, username, passhash, orgid } = process.env;
+    const result = await api.post(`/account/v1.0/token?appId=${appid}`, {
+      appSecret: secret,
+      email: username,
+      orgId: orgid,
+      password: passhash,
+    });
+    const { access_token } = result.data;
+    api.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+    return access_token;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getStationIds = async () => {
-  const result = await api.post(`/station/v1.0/list`, { size: 20, page: 1 });
-  const { total, stationList } = result.data;
-  let idList = [];
-  for (let i = 0; i < total; i++) {
-    const { id } = stationList[i];
-    idList.push(id);
+  try {
+    const result = await api.post(`/station/v1.0/list`, { size: 20, page: 1 });
+    const { total, stationList } = result.data;
+    let idList = [];
+    for (let i = 0; i < total; i++) {
+      const { id } = stationList[i];
+      idList.push(id);
+    }
+    return idList;
+  } catch (error) {
+    console.error(error);
   }
-  return idList;
 };
 
 const getDeviceList = async (stationId) => {
-  const result = await api.post(`/station/v1.0/device`, { stationId });
-  const data = result.data;
-  return data;
+  try {
+    const result = await api.post(`/station/v1.0/device`, { stationId });
+    const data = result.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getDeviceData = async (deviceSn) => {
-  const result = await api.post(`/device/v1.0/currentData`, { deviceSn });
-  const data = result.data;
-  return data;
+  try {
+    const result = await api.post(`/device/v1.0/currentData`, { deviceSn });
+    const data = result.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getPlantData = async (stationId) => {
-  const result = await api.post(`/station/v1.0/realTime`, { stationId });
-  const data = result.data;
-  return data;
+  try {
+    const result = await api.post(`/station/v1.0/realTime`, { stationId });
+    const data = result.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
